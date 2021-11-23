8f29ccc20f5a78ae821edd665deb4d39d9873424
Dahln Farnes <dahln@outlook.com>
2021-11-22 20:07:26 -0700

    #6 Update program.cs and startup.cs to use minimal code

--- BlazorWASM_CRUD_B2C.API/Program.cs
+++ BlazorWASM_CRUD_B2C.API/Program.cs
@@ -1,26 +1,58 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorWASM_CRUD_B2C.Data;
+using BlazorWASM_CRUD_B2C.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
 
-namespace BlazorWASM_CRUD_B2C.API
+//Build App
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DBContext>(options =>
+    options.UseSqlite($"Data Source=dotnetBUILT_BlazorWASM_CRUD_B2C.db"));
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    // ^^^ use this option for MSSQL instead of Sqlite ^^^
+    // If MSSQL is used, remove 'UseSqlite'
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<CustomerService>();
+
+const string corsName = "CORSPolicy";
+builder.Services.AddCors(opt =>
 {
-    public class Program
+    opt.AddPolicy(name: corsName, policy =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        policy.WithOrigins(builder.Configuration.GetSection("Cors").Value)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+
+//Config App
+var app = builder.Build();
+
+//Check for and automatically apply pending migrations
+var db = app.Services.GetService<DbContext>();
+if (db != null)
+{
+    var migrations = db.Database.GetPendingMigrations();
+    if (migrations.Count() > 0)
+        db.Database.Migrate();
 }
+
+app.UseCors(corsName);
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- BlazorWASM_CRUD_B2C.Web/Program.cs
+++ BlazorWASM_CRUD_B2C.Web/Program.cs
@@ -1,3 +1,5 @@
+using BlazorWASM_CRUD_B2C.Web;
+using BlazorWASM_CRUD_B2C.Web.Services;
 using Blazored.LocalStorage;
 using Blazored.Modal;
 using Blazored.Toast;
@@ -6,53 +8,39 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using BlazorWASM_CRUD_B2C.Web.Services;
 
-namespace BlazorWASM_CRUD_B2C.Web
-{
-    public class Program
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddHttpClient("BlazorWASM_CRUD_B2CAPI", client => client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("API_URL")))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>()
+    .AddHttpMessageHandler(sp =>
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+        var handler = sp.GetService<AuthorizationMessageHandler>()
+            .ConfigureHandler(
+                authorizedUrls: new[] { builder.Configuration.GetValue<string>("API_URL") }, //<--- The URI used by the Server project.
+                scopes: new[] { builder.Configuration.GetValue<string>("Scope") });
+        return handler;
+    });
 
-            builder.Services.AddHttpClient("BlazorWASM_CRUD_B2CAPI", client => client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("API_URL")))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>()
-                .AddHttpMessageHandler(sp =>
-                {
-                    var handler = sp.GetService<AuthorizationMessageHandler>()
-                        .ConfigureHandler(
-                            authorizedUrls: new[] { builder.Configuration.GetValue<string>("API_URL") }, //<--- The URI used by the Server project.
-                            scopes: new[] { builder.Configuration.GetValue<string>("Scope") });
-                    return handler;
-                });
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWASM_CRUD_B2CAPI"));
 
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWASM_CRUD_B2CAPI"));
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration.GetValue<string>("Scope"));
 
-            builder.Services.AddMsalAuthentication(options =>
-            {
-                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-                options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration.GetValue<string>("Scope"));
+    options.ProviderOptions.LoginMode = "redirect";
+});
 
-                options.ProviderOptions.LoginMode = "redirect";
-            });
+builder.Services.AddScoped<API>();
+builder.Services.AddScoped<SpinnerService>();
 
-            builder.Services.AddScoped<API>();
-            builder.Services.AddScoped<SpinnerService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
+builder.Services.AddBlazoredModal();
 
-            builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddBlazoredToast();
-            builder.Services.AddBlazoredModal();
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
